89d53e29a39aa2bb98b87da2c5a6f6f06508b94f
11Kclarke <hr19845@bristol.ac.uk>
2022-03-21 13:28:35 +0000

    added N dimensional ode system functonality
    added N dimensional ode system functonality

--- ODEFUNC.py
+++ ODEFUNC.py
@@ -7,8 +7,9 @@ from scipy.integrate import odeint
 from inspect import isfunction
 x,y,t = sp.symbols('x,y,t', real=True)
 
-def euler_step(dxdt,t,stepsize,fill=0):
-    return dxdt(t,fill)*stepsize
+def euler_step(dxdt,x,stepsize,t=0):
+    #print(t)
+    return dxdt(t,x)*stepsize
 
 
 def euler_solve(f,y0,tvals):
@@ -22,10 +23,10 @@ def euler_solve(f,y0,tvals):
 
 def rk4step(f,x,stepsize,t=0):
     h=stepsize
-    k1 = h*f(x,t)
-    k2 = h*f(x+h*k1/2,t+h/2)
-    k3 = h*f(x+h*k2/2,t+h/2)
-    k4 = h*f(x+h*k3,t+h)
+    k1 = h*f(t,x)
+    k2 = h*f(t+h/2,x+h*k1/2)
+    k3 = h*f(t+h/2,x+h*k2/2)
+    k4 = h*f(t+h,x+h*k3)
     return 1/6*(k1+2*k2+2*k3+k4)
        
 def rk4(f,tvals,y0):
@@ -36,21 +37,47 @@ def rk4(f,tvals,y0):
         sol[i+1]=rk4step(f,tvals[i],tvals[i]-tvals[i+1])+sol[i]
     return sol
 
-def Solve_ode(f,tvals,y0,method=euler_step):
+def Solve_ode(f,tvals,y0,method=rk4step):
     steps = len(tvals)
-    sol = np.zeros(steps)
+    print(y0)
+    """
+    if not isfunction(f):
+        if isfunction(f[0]):
+            
+    """
+    #print(np.shape(f([tvals[0],tvals[0]],0)))
+    #tvals=np.array([tvals,tvals],dtype=float)
+    #x = np.zeros(shape=(len(solve_for), initial_condition.size))
+    
+
+
+    sol = np.zeros(shape=(len(y0),len(tvals))).T
+    print(np.shape(sol))
+    print(np.shape(sol[0]))
     sol[0]=y0
+    print(sol[0])
+    #print(f)
     for i in range(steps-1):
-        sol[i+1]=method(f,tvals[i],tvals[i]-tvals[i+1])+sol[i]
+        sol[i+1]=method(f,sol[i],tvals[i]-tvals[i+1],t=tvals[i])+sol[i]
     return sol
 
 
-def Solve_to(f,x0,t0,tn,deltat_max=0.001,method=rk4step,initialvalue=True,t_innitial_condition = None):
+def Solve_to(f,x0,t0,tn,deltat_max=0.001,method=rk4step,initialvalue = True ,t_innitial_condition = None):
+    if type(f)==str:
+        f= sp.lambdify([x,t],sp.parse_expr(f))
+        """
     if not isfunction(f):#attempts to parse function if input isnt already func
-        f= sp.lambdify(x,t,sp.parse_expr(f))
+     try:
+            if len(f)>1  and len(x0)==len(f):
+                sols=[]
+                for i in range(len(f)):
+                    sols.append(Solve_to(f[i],x0[i],t0,tn))
+                return sols
+        #except:
+                #raise "f is not function, iterable of function, or iterable of string represntation of function" 
+    """
     if abs(deltat_max)>abs(t0-tn):
         raise "max step size larger than t range"
-    
     steps = abs(int((t0-tn)/deltat_max))
     finalstepsize = tn-(t0+deltat_max*steps)
     tvals=[t0]*steps
@@ -63,24 +90,51 @@ def Solve_to(f,x0,t0,tn,deltat_max=0.001,method=rk4step,initialvalue=True,t_inni
     for i in range(steps-1):
         tvals[i+1]=deltat_max+tvals[i]
     tvals.append(tvals[-1]+finalstepsize)
+    #tvals = np.vstack((tvals,tvals)).T
+    print(np.shape(tvals))
+    #tvals= [tvals]*len(x0)
     return  (Solve_ode(f,tvals,x0,method=method),tvals)
     
-        
+def dx_dt(t, X):
+    
+    X_dot = np.array([X[1], -X[0]])
+    return X_dot
 
-   
+
+
+def dx_dt2(t, X, a=0.1, b=0.1, d=0.1):
+    x = X[0]
+    y = X[1]
+
+    dxdt = x*(1-x) - a*x*y/(d+x)
+    dydt = b *y *(1 - y/x)
+
+    dXdt = np.array([dxdt, dydt])
+    return dXdt
+
+  
 
 if __name__ == "__main__":
     #(x+5)*(x+1)*(x+3)**2
     f=sp.lambdify([x,t],(x+1)**2)
-    a=Solve_to(f,1,-2,2,initialvalue=False,t_innitial_condition=0)
-    print(np.shape(a))
+    h=sp.lambdify([x,t],t)
+    
+    g = sp.lambdify([x,t],-x**2)
+    f=[h,g]
+    #dx_dt(0,)
+    a=Solve_to(dx_dt2,[0,1],np.pi,3*np.pi)
+    #print(np.shape(a))
     (xvals,tvals)=a
+    
     print(np.shape(xvals))
     print(np.shape(tvals))
-    print(tvals[-1])
-    plt.plot(tvals, xvals)
-    plt.grid()
-    original = f(tvals,0)
+    #print(tvals[-1])
+    
+   
+    plt.plot(xvals[:,1])
 
-    plt.plot(tvals,original)
+    plt.grid()
+    #original = f(tvals,0)
+
+    #plt.plot(tvals,original)
     plt.show()
