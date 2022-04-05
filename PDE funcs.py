c219b53547cdd2902206eb4c8c1f7f8c8a8ed4f5
11Kclarke <hr19845@bristol.ac.uk>
2022-04-05 16:27:19 +0100

    Added dirichelet boundary condition functionality

--- PDE funcs.py
+++ PDE funcs.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 # Set problem parameters/functions
 kappa = 1   # diffusion constant
-L=1.0         # length of spatial domain
-T=0.5         # total time to solve for
+L=5         # length of spatial domain
+T=1        # total time to solve for
 def u_I(x):
     # initial temperature distribution
     y = np.sin(pi*x/L)
@@ -95,32 +95,32 @@ def solvepde(f,t0,tn,domain,condition,deltat_max,method=forwardeuler):
     for i in range(len(domain)):
         domain[i]=np.linspace(domain[i][0],domain[i][1],steps)
     return method()
-def backwardseuler(T,X,innitial,boundary=0):
+def backwardseuler(T,X,innitial,boundary=lambda  t : (0,0)):
     mx = len(X)
     sol = np.zeros(shape=(T.size,X.size))
+    lmbda = kappa*(T[1]-T[0])/((X[1]-X[0])**2)
     for i in range(mx):
         sol[0][i] = innitial(X[i])
-    sol[0][0]=0
-    sol[0][-1]=0
-    print(sol.shape)
-    lmbda = kappa*(T[1]-T[0])/((X[1]-X[0])**2)
+    sol[0][0]=sol[i][-1]+lmbda*boundary(T[i])[0]
+    sol[0][-1]=sol[i][-1]+lmbda*boundary(T[i])[1]
     d = np.array([2*lmbda+1]*(mx))
     d1 = np.array([-lmbda]*(mx-1))
     d2 = np.array([-lmbda]*(mx-1))
     
     for i in range(1,len(T)):
         sol[i]=TDMAsolver(d1,d,d2,sol[i-1])
-        sol[i][0]=0
-        sol[i][-1]=0
+        sol[i][0]=sol[i][-1]+lmbda*boundary(T[i])[0]
+        sol[i][-1]=sol[i][-1]+lmbda*boundary(T[i])[1]
     return sol
-def forwardeuler(T,X,innitial,boundary=0):
+def forwardeuler(T,X,innitial,boundary=lambda  t : (0,0)):
     mx = len(X)
     sol = np.zeros(shape=(T.size,X.size))
+    lmbda = kappa*(T[1]-T[0])/((X[1]-X[0])**2)
     for i in range(mx):
         sol[0][i] = innitial(X[i])
-    sol[0][0]=0
-    sol[0][-1]=0
-    lmbda = kappa*(T[1]-T[0])/((X[1]-X[0])**2)
+    sol[0][0]=sol[i][-1]+lmbda*boundary(T[i])[0]
+    sol[0][-1]=sol[i][-1]+lmbda*boundary(T[i])[1]
+    
     d = np.diag([1-2*lmbda]*(mx))
     d1 = np.diag([lmbda]*(mx-1),k=1)
     d2 = np.diag([lmbda]*(mx-1),k=-1)
@@ -128,16 +128,18 @@ def forwardeuler(T,X,innitial,boundary=0):
     
     for i in range(1,len(T)):
         sol[i]=np.matmul(A,sol[i-1])
+        sol[i][0]=sol[i][-1]+lmbda*boundary(T[i])[0]
+        sol[i][-1]=sol[i][-1]+lmbda*boundary(T[i])[1]
     return sol
-def CrankNicolson(T,X,innitial,boundary=0):
+def CrankNicolson(T,X,innitial,boundary=lambda  t : (0,0)):
     mx = len(X)
+    lmbda = kappa*(T[1]-T[0])/((X[1]-X[0])**2)
     sol = np.zeros(shape=(T.size,X.size))
     for i in range(mx):
         sol[0][i] = innitial(X[i])
-    sol[0][0]=0
-    sol[0][-1]=0
-    print(sol.shape)
-    lmbda = kappa*(T[1]-T[0])/((X[1]-X[0])**2)
+    sol[0][0]=sol[i][-1]+lmbda*boundary(T[i])[0]
+    sol[0][-1]=sol[i][-1]+lmbda*boundary(T[i])[1]
+
     d = np.diag([lmbda-1]*(mx))
     d1 = np.diag([lmbda/2]*(mx-1),k=1)
     d2 = np.diag([lmbda/2]*(mx-1),k=-1)
@@ -149,24 +151,31 @@ def CrankNicolson(T,X,innitial,boundary=0):
         
         sol[i]=np.matmul(A,sol[i-1])
         sol[i]=TDMAsolver(d1b,db,d2b,sol[i])
-        sol[i][0]=0
-        sol[i][-1]=0
+        sol[i][0]=sol[i][-1]+lmbda*boundary(T[i])[0]
+        sol[i][-1]=sol[i][-1]+lmbda*boundary(T[i])[1]
     return sol
 if __name__ == "__main__":
-    fig, axs = plt.subplots(2)
-    """ xvals = np.linspace(0,1,200)
-    tvals = np.linspace(0,1,200)
-    correct =  u_exact(xvals,tvals)
+    fig, axs = plt.subplots(4)
+    xvals = np.linspace(0,L,5)
+    tvals = np.linspace(0,T,200)
+    correct = []
+    for i in xvals:
+        correct.append(u_exact(i,tvals))
+    correct=np.array(correct).T
     
-    axs[0].plot(tvals,correct)"""
-    xvals = np.linspace(0,5,10)
-    tvals = np.linspace(0,1,100)
-    X=backwardseuler(tvals,xvals,u_I)
+    print(correct.shape)
+    axs[3].plot(correct)
+
+    X=forwardeuler(tvals,xvals,u_I)
     print(X.shape)
     axs[0].plot(X)
-    X=CrankNicolson(tvals,xvals,u_I)
+    X=backwardseuler(tvals,xvals,u_I)
     print(X.shape)
     axs[1].plot(X)
+    X=CrankNicolson(tvals,xvals,u_I)
+    print(X.shape)
+    axs[2].plot(X)
+   
     for i in axs:
         i.grid()
     plt.show()
